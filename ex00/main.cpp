c88b8cb1e7fd29cbeb45526c761fe56e22f4e572
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-03-06 11:38:35 +0100

    add test from subject

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,21 +1,43 @@
 #include "whatever.hpp"
+#include "terminalOutput.hpp"
 #include <iostream>
 
 int main(void)
 {
-    int a = 1337;
-    int b = 69;
-    std::cout << "a: " << a << ". b: " << b << ".\n";
-    std::cout << "min " << min<int>(a, b) << ". max: " << max<int>(a, b) << ".\n";
-    swap<int>(a, b);
-    std::cout << "a: " << a << ". b: " << b << ".\n";
-    std::cout << "min " << min<int>(a, b) << ". max: " << max<int>(a, b) << ".\n";
+    // subject test
+    std::cout << C_BLUE << "Subject test" << C_END << std::endl;
+    {
+        int a = 2;
+        int b = 3;
+        ::swap(a, b);
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+        std::string c = "chaine1";
+        std::string d = "chaine2";
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+        std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    }
 
-    float c = 3.14;
-    float d = 1.001;
-    std::cout << "c: " << c << ". d: " << d << ".\n";
-    std::cout << "min " << min<float>(c, d) << ". max: " << max<float>(c, d) << ".\n";
-    swap<float>(c, d);
-    std::cout << "c: " << c << ". d: " << d << ".\n";
-    std::cout << "min " << min<float>(c, d) << ". max: " << max<float>(c, d) << ".\n";
+    // extra test
+    std::cout << C_BLUE << "Extra test" << C_END << std::endl;
+    {
+        int a = 1337;
+        int b = 69;
+        std::cout << "a: " << a << ". b: " << b << ".\n";
+        std::cout << "min " << min<int>(a, b) << ". max: " << max<int>(a, b) << ".\n";
+        swap(a, b);
+        std::cout << "a: " << a << ". b: " << b << ".\n";
+        std::cout << "min " << min<int>(a, b) << ". max: " << max<int>(a, b) << ".\n";
+
+        float c = 3.14;
+        float d = 1.001;
+        std::cout << "c: " << c << ". d: " << d << ".\n";
+        std::cout << "min " << min<float>(c, d) << ". max: " << max<float>(c, d) << ".\n";
+        swap(c, d);
+        std::cout << "c: " << c << ". d: " << d << ".\n";
+        std::cout << "min " << min<float>(c, d) << ". max: " << max<float>(c, d) << ".\n";
+    }
 }
